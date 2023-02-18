269bfc4ef890ea4fbdb3f905ffb3422f67da1983
ThomasHardy98 <tj.hardy77@yahoo.co.uk>
2023-02-18 21:26:16 +0000

    Almost finished styling and structuring TV layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,6 @@ const Header = () => {
     <header className="header-container">
       <h1 className="name-heading">Thomas Hardy</h1>
       <p className="short-description">React Developer</p>
-      <ul>
-        <li>Home</li>
-        <li>Projects</li>
-        <li>Contact</li>
-      </ul>
     </header>
   );
 };
--- src/components/TV/TV.tsx
+++ src/components/TV/TV.tsx
@@ -23,23 +23,33 @@ const TV = () => {
       <div id="tv-border"></div>
       <div id="border-effect"></div>
       <div className="tv__details">
-        <div className="tv__audio"></div>
         <div className="tv__info">
-          <h3>SONY</h3>
-          <div className="tv__controls">
-            <button className="effects-button" onClick={toggleEffects}>
-              {effCtx.useRetro
-                ? "Disable Retro Effects"
-                : "Enable Retro Effects"}
+          <h3>FONY</h3>
+        </div>
+        <div className="tv__controls">
+          <div className="button-container">
+            <button className="tv-button" onClick={toggleEffects}>
+              <div className="button-shine" />
             </button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+            <label className="effects-button-label">Effects</label>
+          </div>
+          <div className="button-container">
+            <button className="tv-button">
+              <div className="button-shine" />
+            </button>
+            <label className="effects-button-label">Projects</label>
+          </div>
+          <div className="button-container">
+            <button className="tv-button">
+              <div className="button-shine" />
+            </button>
+            <label className="effects-button-label">Home</label>
+          </div>
+          <div className="power-container">
+            <div className="power-light" />
+            <img className="power-icon" src="src\assets\power.png" />
           </div>
         </div>
-        <div className="tv__audio"></div>
       </div>
       <div id="effects" className={className}></div>
     </div>
