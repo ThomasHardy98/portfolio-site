360a9b992a63c4931e37b3d235fbbbe57fdd9d89
ThomasHardy98 <tj.hardy77@yahoo.co.uk>
2023-02-19 00:02:26 +0000

    Added optional text glow to effects

--- src/Page/Page.tsx
+++ src/Page/Page.tsx
@@ -1,11 +1,24 @@
+import { useContext } from "react";
+
+import { EffectsContext } from "~/context/EffectsContext";
+
 import Header from "components/Header/Header";
 import Footer from "components/Footer/Footer";
 
 import "Page/Page.scss";
 
 const Page = () => {
+  const effCtx = useContext(EffectsContext);
+  let className = "";
+
+  if (effCtx.useRetro) {
+    className += "text-glow";
+  } else {
+    className += "";
+  }
+
   return (
-    <div className="content-container">
+    <div className={`content-container ${className}`}>
       <Header />
       <main className="main-container">
         <div>Test Text</div>
